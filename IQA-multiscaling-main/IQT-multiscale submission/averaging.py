378f1480ba1292a656d37d6ddce664307e318043
KomalPal9610 <102805678+KomalPal9610@users.noreply.github.com>
2022-04-03 17:27:53 +0530

    Update averaging.py

--- IQA-multiscaling-main/IQT-multiscale submission/averaging.py
+++ IQA-multiscaling-main/IQT-multiscale submission/averaging.py
@@ -10,10 +10,10 @@ Original file is located at
 import pandas as pd
 
 #We have mannualy added the column name A,B for the reference
-df1 = pd.read_csv("Test0.5.txt")
-df2 = pd.read_csv("Test1.txt")
-df3 = pd.read_csv("Test2.txt")
-df4 = pd.read_csv("Test3.txt")
+df1 = pd.read_csv("outputScale0.5.txt")
+df2 = pd.read_csv("outputScale1.txt")
+df3 = pd.read_csv("outputScale2.txt")
+df4 = pd.read_csv("outputScale3.txt")
 df5 = pd.concat([df1['A'],(df1['B'] + df2['B'] + df3['B']+df4['B'])/4],axis = 1)
 df5.to_csv("output.txt", index = False)
 #After saving the output file one can remove the first like containing A and B as column name
